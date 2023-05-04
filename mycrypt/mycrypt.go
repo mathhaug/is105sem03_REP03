9f042d4f577425ff33d5cc79f7305011a9f6b13e
Mathias Haugen <haugen1997@gmail.com>
2023-05-04 03:49:03 +0200

    h

--- mycrypt/mycrypt.go
+++ mycrypt/mycrypt.go
@@ -1,25 +1,36 @@
 package mycrypt
 
+import (
+	"unicode"
+)
+
 var ALF_SEM03 = []rune("abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ0123456789.,:; ")
 
 func Krypter(melding []rune, alphabet []rune, chiffer int) []rune {
 	kryptertMelding := make([]rune, len(melding))
 	for i := 0; i < len(melding); i++ {
 		indeks := sokIAlfabetet(melding[i], alphabet)
-		if indeks+chiffer >= len(alphabet) {
-			kryptertMelding[i] = alphabet[indeks+chiffer-len(alphabet)]
+		isUppercase := unicode.IsUpper(melding[i])
+		if isUppercase {
+			indeks += 44 // convert uppercase to lowercase index
+		}
+		indeks = (indeks + chiffer) % len(alphabet)
+		if indeks < 0 {
+			indeks += len(alphabet)
+		}
+		if isUppercase {
+			kryptertMelding[i] = unicode.ToUpper(alphabet[indeks])
 		} else {
-			kryptertMelding[i] = alphabet[indeks+chiffer]
+			kryptertMelding[i] = alphabet[indeks]
 		}
 	}
 	return kryptertMelding
 }
 
-func sokIAlfabetet(symbol rune, alfabet []rune) int {
-	for i := 0; i < len(alfabet); i++ {
-		if symbol == alfabet[i] {
+func sokIAlfabetet(symbol rune, alphabet []rune) int {
+	for i := 0; i < len(alphabet); i++ {
+		if symbol == alphabet[i] {
 			return i
-			break
 		}
 	}
 	return -1
